d6611ec778de9013c570d4c29e27134c55571437
MD Motasim Billah <themotasimdev@gmail.com>
2026-05-10 15:47:56 +0600

    schema folder created

--- express 3rd/server.js
+++ express 3rd/server.js
@@ -1,8 +1,10 @@
+const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+//mongodb+srv://mern2407:<db_password>@cluster0.ptmdayy.mongodb.net/?appName=Cluster0
 
 const user = [
   {
@@ -29,9 +31,9 @@ app.get("/user", (req, res) => {
 app.post("/user", (req, res) => {
   // res.json(user);
   // res.send(user);
-  const some = req.body
-  console.log(some)
-  res.json("ok done")
+  const some = req.body;
+  console.log(some);
+  res.json("ok done");
 });
 
 app.listen(port, () => {
