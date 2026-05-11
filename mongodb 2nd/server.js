16ca7c0a7ac7ad18a81e58cd86be552952292dac
MD Motasim Billah <themotasimdev@gmail.com>
2026-05-11 08:06:53 +0600

    bcrypt

--- mongodb 2nd/server.js
+++ mongodb 2nd/server.js
@@ -1,13 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
+//password hashing,
 
+const bcrypt = require("bcrypt");
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./model/userSchema");
+const app = express();
+const port = 3000;
 
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+mongoose
+  .connect(
+    "mongodb+srv://mern2407:motasim@cluster0.yinvml1.mongodb.net/mern2407?appName=Cluster0",
+  )
+  .then(console.log("connect"));
+
+app.post("/user", (req, res) => {
+  const { name, email, password } = req.body;
+
+  bcrypt.hash(password, 10, function (err, hash) {
+    // if (err) {
+    //   console.error(err);
+    //   return;
+    // }
+    console.log(hash);
+    const user = new User({
+      name: name,
+      email: email,
+      password: hash,
+    });
+    user.save();
+  });
+  res.send("data gese");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
